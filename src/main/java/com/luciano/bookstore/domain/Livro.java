4560ac3a58d3ebf77c26385aaa169fe0ae773ed1
lufranca <lucyano_spfc@hotmail.com>
2021-07-21 21:38:48 -0300

    finalizando classes de dominio

--- src/main/java/com/luciano/bookstore/domain/Livro.java
+++ src/main/java/com/luciano/bookstore/domain/Livro.java
@@ -1,19 +1,32 @@
 package com.luciano.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class Livro implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nome_autor;
 	private String texto;
-
+	
+	@ManyToOne
+	@JoinColumn(name="categoria_id	")
 	private Categoria catergoria;
 
 	public Livro() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public Livro(Integer id, String titulo, String nome_autor, String texto, Categoria catergoria) {
