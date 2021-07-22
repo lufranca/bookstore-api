2bf6f27ce200fe5c0a3be42d72e299bdecc33248
lufranca <lucyano_spfc@hotmail.com>
2021-07-21 23:19:21 -0300

    Criacao perfil de DEV

--- src/main/java/com/luciano/bookstore/config/DevConfig.java
+++ src/main/java/com/luciano/bookstore/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.luciano.bookstore.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,15 +9,23 @@ import org.springframework.context.annotation.Profile;
 import com.luciano.bookstore.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
 	@Bean
-	public void instanciaBaseDeDados() {
-		this.dbService.instanciaBaseDeDados();
+	public boolean instanciaBaseDeDados() {
+
+		if (strategy.equals("create")) {
+			this.dbService.instanciaBaseDeDados();
+		}
+		return false;
+
 	}
 
 }
