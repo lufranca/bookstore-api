080a0e7e4a857d5ff1e29e49168787bde40b205a
lufranca <lucyano_spfc@hotmail.com>
2021-07-22 20:36:39 -0300

    Excecao personalizada para objeto nao encontrado

--- src/main/java/com/luciano/bookstore/services/CategoriaService.java
+++ src/main/java/com/luciano/bookstore/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.luciano.bookstore.domain.Categoria;
 import com.luciano.bookstore.repositories.CategoriaRepository;
+import com.luciano.bookstore.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + " Tipo: " + Categoria.class.getName()));
 	}
 }
---
+++ src/main/java/com/luciano/bookstore/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,16 @@
+package com.luciano.bookstore.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ObjectNotFoundException(String message) {
+		super(message);
+	}
+	
+	
+
+}
