142f8314f9c42adddf289d8649f9c74599d010a4
lufranca <lucyano_spfc@hotmail.com>
2021-07-23 23:14:23 -0300

    findAll para categorias utilizando padrao DTO

--- src/main/java/com/luciano/bookstore/domain/Livro.java
+++ src/main/java/com/luciano/bookstore/domain/Livro.java
@@ -9,6 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.ManyToAny;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
--- src/main/java/com/luciano/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/luciano/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.luciano.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luciano.bookstore.domain.Categoria;
+import com.luciano.bookstore.dtos.CategoriaDTO;
 import com.luciano.bookstore.services.CategoriaService;
 
 @RestController
@@ -17,9 +21,16 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
+	@GetMapping(value = "{id}")
+	public ResponseEntity<?> findById(@PathVariable Integer id) {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
