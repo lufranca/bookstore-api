eac08d07bba16a5cf0eca4c44f3b536d02dad75c
lufranca <lucyano_spfc@hotmail.com>
2021-07-24 19:05:02 -0300

    POST para Livro

--- src/main/java/com/luciano/bookstore/services/LivroService.java
+++ src/main/java/com/luciano/bookstore/services/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.luciano.bookstore.domain.Categoria;
 import com.luciano.bookstore.domain.Livro;
 import com.luciano.bookstore.repositories.LivroRepository;
 import com.luciano.bookstore.services.exception.ObjectNotFoundException;
@@ -40,6 +41,13 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCatergoria(cat);
+		return repository.save(obj);
 	}
 
 }
