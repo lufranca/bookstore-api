e99736fa2432285bc30076148f6b63968dda3a4e
lufranca <lucyano_spfc@hotmail.com>
2021-07-24 15:47:39 -0300

    UPDATE para Categoria

--- src/main/java/com/luciano/bookstore/services/CategoriaService.java
+++ src/main/java/com/luciano/bookstore/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luciano.bookstore.domain.Categoria;
+import com.luciano.bookstore.dtos.CategoriaDTO;
 import com.luciano.bookstore.repositories.CategoriaRepository;
 import com.luciano.bookstore.services.exception.ObjectNotFoundException;
 
@@ -28,6 +29,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+		Categoria obj = findById(id);
+		obj.setNome(objDTO.getNome());
+		obj.setDescricao(objDTO.getDescricao());
 		return repository.save(obj);
 	}
 }
