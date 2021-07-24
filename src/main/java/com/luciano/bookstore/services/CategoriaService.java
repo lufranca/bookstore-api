d1130bda820718befe9016d23deb36a80b697007
lufranca <lucyano_spfc@hotmail.com>
2021-07-24 16:11:59 -0300

    excecao personalizada para delete de Categoria

--- src/main/java/com/luciano/bookstore/services/CategoriaService.java
+++ src/main/java/com/luciano/bookstore/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.luciano.bookstore.domain.Categoria;
@@ -26,7 +27,7 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
@@ -41,6 +42,12 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		findById(id);
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.luciano.bookstore.services.exception.DataIntegrityViolationException(
+					"Categoria não pode ser deletada! Possui livros associados!");
+		}
+
 	}
 }
